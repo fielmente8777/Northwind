2d34104f04f04eb17bd11b1c1b743579730395cc
Hem Bhadur <sushilkc2611@gmail.com>
2025-06-20 15:41:42 +0530

    add popup for discount

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Nunito } from "next/font/google";
 import "./globals.scss";
 import RenderChatBot from "@/components/chatbot/RenderChatBot";
+import DiscountPopup from "@/components/discountPopup/DiscountPopup";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,30 @@ export const metadata: Metadata = {
     "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa, inventore.",
 };
 
+const data = {
+  contact: {
+    href: "tel:+919317207373",
+    label: "Call Us",
+    bgColor: "#D4B01C",
+    textColor: "#fff",
+  },
+  content: {
+    // discount: 10,
+    _html: `<p class="content-para">On same day <span class="purple-purse">booking</span></p>`,
+    // subTitle: "On same day booking",
+    title: "RESERVE TODAY",
+    borderColor: "#fff",
+  },
+  logo: {
+    alt: "Luxury Vibe Stay",
+    src: "/favicon.ico",
+  },
+  image: {
+    alt: "Luxury Vibe Stay",
+    src: "/contact.png",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -31,6 +56,7 @@ export default function RootLayout({
         suppressHydrationWarning={true}
       >
         <RenderChatBot />
+        <DiscountPopup {...data} />
         {children}
       </body>
     </html>
